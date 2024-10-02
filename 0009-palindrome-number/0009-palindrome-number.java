df4f1b433d3ceeb45e11eeaabb11a8874fe4c7c5
Partha Sarathy Mohanty <mohantyparthasarathy04@gmail.com>
2024-10-02 20:42:38 +0530

    Time: 5 ms (71.8%), Space: 43.7 MB (77.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,37 +1,27 @@
 import java.util.Scanner;  // Import the Scanner class
-
 class Solution {
     public boolean isPalindrome(int org) {
-        // Negative numbers are not palindromes
-        if (org < 0) {
-            return false;
+        int rev=0,temp;
+        temp=org;
+        for(;org>0;){
+            int digit =org%10;
+            rev=rev*10+digit;
+            org=org/10;
         }
-
-        int rev = 0, temp = org;
-
-        // Loop to reverse the number
-        while (org > 0) {
-            int digit = org % 10;  // Get the last digit
-            rev = rev * 10 + digit;  // Add the digit to reversed number
-            org = org / 10;  // Remove the last digit from original number
-        }
-
-        // Check if reversed number is the same as the original
-        return (rev == temp);
+        return(rev==temp);
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  // Create Scanner object
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         // Get user input
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
 
-        Solution sol = new Solution();  // Create instance of Solution class
-
-        // Output true or false based on whether the number is a palindrome
+        Solution sol = new Solution();
+        
+        // Output true or false
         System.out.println(sol.isPalindrome(num));
 
-        scanner.close();  // Close the Scanner object
+        scanner.close();
     }
 }
